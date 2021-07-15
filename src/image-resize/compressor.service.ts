dd60ca2ca76d7479b6dea5a54f3e401e129c3547
sang-w0o <robbyra@gmail.com>
2021-07-16 08:03:14 +0900

    chore: Refactor compress function

--- src/image-resize/compressor.service.ts
+++ src/image-resize/compressor.service.ts
@@ -1,23 +1,26 @@
-import sharp, { OutputInfo } from 'sharp';
+import sharp, { OutputInfo } from "sharp";
 
 export interface CompressOptions {
-    width: number;
-    height: number;
+  width: number;
+  height: number;
 }
 
 interface ImageDataAndInfo {
-    data: Buffer;
-    info: OutputInfo;
+  data: Buffer;
+  info: OutputInfo;
 }
 
-export async function compress(bits: Buffer, options: CompressOptions): Promise<ImageDataAndInfo> {
-    const { data, info } = await sharp(bits)
+export const compress = async (
+  bits: Buffer,
+  options: CompressOptions
+): Promise<ImageDataAndInfo> => {
+  const { data, info } = await sharp(bits)
     .resize(options.width, options.height)
     .webp()
     .toBuffer({ resolveWithObject: true });
 
-    return {
-        data,
-        info,
-    }
-}
+  return {
+    data,
+    info,
+  };
+};
