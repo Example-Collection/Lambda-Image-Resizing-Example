6632676c3947035bffa37e2c8096d47205938f0f
sang-w0o <robbyra@gmail.com>
2021-07-16 09:11:03 +0900

    feat: Configure S3

--- src/image-resize/handler.ts
+++ src/image-resize/handler.ts
@@ -1,13 +1,30 @@
 import { Handler } from "aws-lambda";
 import * as AWS from "aws-sdk";
+import {
+  AWS_S3_ACCESS_KEY_ID,
+  AWS_S3_BUCKET,
+  AWS_S3_SECRET_ACCESS_KEY,
+} from ".";
 
 interface Response {
   message: string;
 }
 
-const helloLambdaWorld: Handler = async () => {
+const upload: Handler = async (event) => {
+  AWS.config.update({
+    region: "ap-northeast-2",
+    credentials: new AWS.Credentials({
+      accessKeyId: AWS_S3_ACCESS_KEY_ID,
+      secretAccessKey: AWS_S3_SECRET_ACCESS_KEY,
+    }),
+  });
+
+  console.log({ event });
+
+  const s3 = new AWS.S3({ params: { Bucket: AWS_S3_BUCKET } });
+
   const response: Response = {
-    message: "Hello, Lambda new!",
+    message: "Hello, Lambda new!!",
   };
 
   return {
@@ -16,4 +33,4 @@ const helloLambdaWorld: Handler = async () => {
   };
 };
 
-export { helloLambdaWorld };
+export { upload };
